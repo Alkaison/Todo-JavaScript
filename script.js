3e6f764baef73fc12c8cf38feeeef8e8c8cde9fc
Alkaison <505ganeshmourya@gmail.com>
2023-06-13 18:17:01 +0530

    Feat: validate newTask message and add it to list

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// ToDo App Logic 
+const taskInput = document.querySelector("#taskInput");
+const taskAddBtn = document.querySelector("#addSvg");
+const taskContainer = document.querySelector(".task-container");
+const taskField = document.querySelector(".task");
+const clearAllTaskBtn = document.querySelector("#clearAllTaskBtn");
+
+// add the task when click 
+taskAddBtn.addEventListener("click", () => {
+    // get input field value 
+    const newTask = taskInput.value;
+
+    if(newTask !== '' && newTask.length >= 5)
+    {
+        // cloning the structure of tasks 
+        const newTaskField = taskField.cloneNode(true);
+
+        // select the task text field 
+        const newTaskText = newTaskField.querySelector(".taskText");
+        newTaskText.textContent = newTask;
+
+        taskContainer.append(newTaskField);
+
+        taskInput.value = '';
+    }
+    else
+        alert("Task must be of at least 5 characters to be registered.");
+
+});
