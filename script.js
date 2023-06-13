61311105d106ee883dce49f27d2f93c6f7bf3054
Alkaison <505ganeshmourya@gmail.com>
2023-06-13 22:16:31 +0530

    Fix: added data values to icons and JS for tasks

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ const taskAddBtn = document.querySelector("#addSvg");
 const taskContainer = document.querySelector(".task-container");
 const taskField = document.querySelector(".task");
 const clearAllTaskBtn = document.querySelector("#clearAllTaskBtn");
+const taskCompletedIcon = "fa-solid fa-circle-check pendingSvg";
+const taskUncompletedIcon = "fa-regular fa-circle pendingSvg";
+
+const completedTask = (task) => {
+
+    // if the task sign is uncheck then check it, visa-versa
+    task.className = task.className === taskUncompletedIcon ? taskCompletedIcon : taskUncompletedIcon;
+}
+
+const editTaskText = (task) => {
+    
+    // make the task text editable 
+    const editTaskText = taskElement.querySelector(".taskText"); 
+    editTaskText.setAttribute("contenteditable", "true");
+}
 
 // add the task when click 
 taskAddBtn.addEventListener("click", () => {
@@ -25,6 +40,33 @@ taskAddBtn.addEventListener("click", () => {
     else
         alert("Task must be of at least 5 characters to be registered.");
 
+});
+
+// check which task is clicked and which button is clicked 
+taskContainer.addEventListener("click", (e) => {
+    // select the particular task 
+    const taskElement = e.target.parentElement;
+
+    if(taskElement.classList.contains("task"))
+    {
+        // get the data value of clicked element 
+        const clickedElement = e.target;
+        const data = clickedElement.getAttribute("data");;
+
+        switch(data)
+        {
+            case 'check':
+                completedTask(clickedElement);
+                break;
+            case 'task':
+                break;
+            case 'edit':
+                editTaskText(clickedElement);
+                break;
+            case 'delete':
+                break;
+        }
+    }
 });
 
 // remove all childs of "taskContainer" class 
